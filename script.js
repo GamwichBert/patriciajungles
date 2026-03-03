979ea7096f0365a39dc8afb0b2a279adda5cb405
GamwichBert <jungles442@gmail.com>
2026-03-03 19:37:31 -0300

    Commit Inicial

--- script.js
+++ script.js
@@ -37,7 +37,7 @@ const stories = {
 
     2: "Lembra do primeiro dia? Da expectativa imensa? E então fomos ao Galgo. Aquele bar foi o nosso Prancing Pony. Tivemos uma pizza quadrada absolutamente incrível e, acima de tudo, o chopp do Iron Maiden. Cá entre nós, sim, ele tem gosto de baunilha, porque afinal, tu tens sempre razão. Foi ali que a nossa jornada juntos realmente começou.",
 
-    3: "Eu sempre soube. Dizem que sou louco, mas eu sinto as energias ao meu redor. Quando atiraram o buquê de casamento, eu não tive dúvidas, o universo já tinha conspirado a nosso favor; aquela magia era nossa. Foi como se tivessem lançado a Profecia e tu, minha linda, pegaste no nosso futuro em forma de flores.",
+    3: "Eu sempre soube. Dizem que sou louco, mas eu sinto as energias ao meu redor. Quando atiraram o buquê de casamento, eu não tive dúvidas, o universo já tinha conspirado a nosso favor. Aquela magia era nossa. Foi como se tivessem lançado a Profecia e tu, minha linda, pegou no nosso futuro em forma de flores.",
 
     4: "O meu Patrono seria com certeza algo que lembrasse o teu sorriso, porque pensar em ti espanta qualquer Dementador da minha vida. Tu és o meu feitiço Lumos nos dias mais escuros. É como dizem em Valfenda: o teu amor traz uma paz e uma força que eu antes desconhecia."
 };
